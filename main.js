24827b9d5bd6ca22d393d45782751c0525853b2f
LU KE-XIN <70186403+lu773414@users.noreply.github.com>
2025-05-29 20:52:51 +0800

    test

---
+++ main.js
@@ -0,0 +1,34 @@
+const { createApp, ref, onMounted  } = Vue
+
+createApp({
+    setup() {
+        const page = ref(1);
+        const answer = ref('');
+        const choose = ref(['','',' ']);
+
+        const setPage = (newPage) => {
+            page.value = newPage;
+        }
+
+        const chooseAnswer = (value) => {
+            choose.value[page.value-1] = value;
+            page.value = page.value +1;
+            answer.value = `./image/${choose.value.join('')}.png`
+        }
+
+        onMounted(() => {
+            setTimeout(() => {
+                setPage(2)
+                console.log("1秒後自動跳到第2頁")
+            }, 1500)
+        })
+
+        return {
+            page,
+            answer,
+            choose,
+            setPage,
+            chooseAnswer
+        }
+    }
+}).mount('#app')
